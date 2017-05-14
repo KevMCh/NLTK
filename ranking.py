b5a194a2a19087c456ddaff118e0707c91b7a722
Kevin <marchinkev@gmail.com>
2017-05-14 10:48:03 +0100

    Created ranking

--- ranking.py
+++ ranking.py
@@ -1,23 +1,31 @@
 import nltk
+import collections
+import operator
 
 nltk.download('punkt')
 nltk.download('maxent_treebank_pos_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 
-string = 'Prince William and his new wife Catherine kissed twice to satisfy the besotted Buckingham Palace crowds'
+string = 'Prince William and his new new wife Catherine new kissed twice to satisfy the besotted Buckingham Palace Palace crowds'
 tokens = nltk.word_tokenize(string)
-print(tokens)
+# print(tokens)
 
 tags = nltk.pos_tag(tokens)
-print(tags)
+# print(tags)
 
-chunkingEn = nltk.ne_chunk(tags, binary=True)
-print(chunkingEn)
+chunkingEn = nltk.ne_chunk(tags, binary = True)
+# print(chunkingEn)
 
 for x in chunkingEn.subtrees():
  if hasattr(x, 'label'):
      if x.label() == "NE":
          words = [w[0] for w in x.leaves()]
          name = " " . join(words)
-         print(name)
+         # print(name)
+
+counterWords = dict(collections.Counter(words))
+
+sortedCounterWords = sorted(counterWords.items(), key = operator.itemgetter(1))
+sortedCounterWords = list(reversed(sortedCounterWords))
+print(sortedCounterWords)
